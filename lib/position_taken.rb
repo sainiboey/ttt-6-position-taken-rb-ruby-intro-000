a04014e3c53f724a684a52c700bed3da42d6f54a
sainiboey <kabising@gmail.com>
2019-11-25 00:03:11 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,9 @@
 def position_taken?(board,index)
   if (board[index] != " ") || (board[index] != "") || (board[index] != nil)
     return false
+  elsif (board[index] == "X") || (board[index] == "O")
+    return true
   end
-  true
+    
 end
 # code your #position_taken? method here!
